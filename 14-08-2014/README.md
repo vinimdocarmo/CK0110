756a0845a6aa1c0ca2598d7da2b0d558ac7297e2
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2014-08-14 13:04:48 -0300

    Modifica notas de aula do dia 14-08-2014

--- 14-08-2014/README.md
+++ 14-08-2014/README.md
@@ -1,4 +1,4 @@
 #CK0110 - PROGRAMAÇÃO
 ##14/08/2014
 ###Abertura da disciplina
-[Primeiro programa em C](https://github.com/vinimdocarmo/CK0110/tree/master/14-08-2014/app.c)
+[Primeiro programa em C](https://github.com/vinimdocarmo/CK0110/tree/master/14-08-2014/app.c): Calcular diferença entre datas (em dias).
--- 14-08-2014/app.c
+++ 14-08-2014/app.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int narg, char *argv[]) {
+int isLeapYear(int year);
 
-  return EXIT_SUCCESS;
+int main(int narg, char *argv[]) {
+  int day1, month1, year1,
+      day2, month2, year2,
+      i, temp, sum=0,
+      month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  printf("Entre com a data 1 : (DD MM YYYY) : ");
+  scanf("%d %d %d", &day1, &month1, &year1);
+
+  printf("Entre com a data 2 : (DD MM YYYY) : ");
+  scanf("%d %d %d", &day2, &month2, &year2);
+
+  temp = day1;
+
+  for(i = month1; i < month2 + (year2 - year1) * 12; i++) {
+    if(i > 12) {
+      i = 1;
+      year1++;
+    }
+
+    if(i == 2) {
+      if(isLeapYear(year1))
+        month[i - 1] = 29;
+      else
+        month[i - 1] = 28;
+    }
+
+    sum = sum + (month[i - 1] - temp);
+    temp = 0;
+  }
+
+  sum = sum + day2 - temp + 1;
+
+  printf("\nDiferença em dias: %i\n",sum);
+}
+
+int isLeapYear(int year) {
+  if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    return 1;
+
+  return 0;
 }
